e0f3fc5dc65c25e4b1a8d630035cea1d9c8248aa
Rahul Kumar <jayantkumarrahul@gmail.com>
2026-08-25 19:37:28 +0530

    Time: 0 ms (100%), Space: 9.7 MB (75.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       sort(s.begin(),s.end()); 
-       sort(t.begin(),t.end()); 
-       if(s == t){
-        return true;
+       int freq[256]= {0};
+       for(int i = 0;i<s.size();i++){
+        freq[s[i]]++;
        }
-       return false;
+       for(int i = 0;i<t.size();i++){
+        freq[t[i]]--;
+       }
+       for(int i = 0;i<256;i++){
+       if(freq[i] != 0){
+        return false;
+       }
+       }
+       return true;
     }
 };

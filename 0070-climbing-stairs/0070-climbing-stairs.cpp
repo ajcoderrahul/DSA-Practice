6bd65085f2fd1f37cb6b578aecd6d7b88e41ece2
Rahul Kumar <jayantkumarrahul@gmail.com>
2026-09-19 14:04:51 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.8%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n ==0 || n == 1) return 1;
+        
+        // return climbStairs(n-1)+climbStairs(n-2);
+        int a = 0;
+        int b= 1;
+        int c= a+b;
+        for(int i = 0;i<n;i++){
+            c= a+b;
+            a = b;
+            b= c;
+        }
+        return b;
+    }
+};

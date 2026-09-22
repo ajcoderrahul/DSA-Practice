6f416fd2ac72a875d7cebf6eab444a9d42d4d017
Rahul Kumar <jayantkumarrahul@gmail.com>
2026-09-22 16:58:51 +0530

    Time: 3 ms (69.19%), Space: 18.9 MB (99.54%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -11,7 +11,7 @@ public:
         }return true;
     }
 
-    bool validPalindrome(string s) {
+    bool validPalindrome(string& s) {
       int i = 0;
       int j = s.length()-1;
       while(i<j){
